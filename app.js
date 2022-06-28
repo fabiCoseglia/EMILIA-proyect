cacf03bbc39ff1385b86699381aea5c96435b37d
fabiCoseglia <fabiancoseglia@gmail.com>
2022-06-28 11:04:48 -0300

    EMILIA desktop designe Home

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require ('path');
+const port = 3030;
+
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/contact', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'views/contact.html'));
+});
+
+app.get ('/music', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/music.html'));
+});
+
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/about.html'));
+});
+
+
+
+
+
+//SERVER//
+app.listen(port,()=>{console.log(`server runing in http//:localhost:${port}`)});
